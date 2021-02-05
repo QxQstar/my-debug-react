a0ab403f5be1464222a81dffcac7e860983d5d93
heyu <1395294694@qq.com>
2021-02-05 10:27:57 +0800

    feat: debug react v17.0.1 环境

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 
 class ClickCounter extends React.Component {
   constructor(props) {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
 import ClickCounter from './App';
 
 ReactDOM.render(
