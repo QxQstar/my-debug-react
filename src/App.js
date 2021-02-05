28223f031bf8f6e4b6d5e587428cbd4f2f44a5e2
heyu <1395294694@qq.com>
2021-02-05 09:28:17 +0800

    style: 清除不重要的代码

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ClickCounter extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {count: 0};
+      this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+      this.setState((state) => {
+          return {count: state.count + 1};
+      });
+  }
+  
+  componentDidUpdate() {}
+
+  render() {
+      return [
+          <button key="1" onClick={this.handleClick}>Update counter</button>,
+          <span key="2">{this.state.count}</span>
+      ]
+  }
 }
 
-export default App;
+export default ClickCounter;
